31eb1e57fa33303b744aed123e8ac4b0bd280289
shawniut <shawniut@gmail.com>
2014-05-13 19:13:13 -0600

    RELREA project is just created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RELREA::Application.config.secret_key_base = '1aa2f253c6512b58eacb82eb7595e44efea3cdff9e62f740c45e37aa656a0d005a7abf7b011f984f5709535a623b304d94fc73bbea22e8379f6dc4036010cb9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RELREA::Application.config.session_store :cookie_store, key: '_RELREA_session'
