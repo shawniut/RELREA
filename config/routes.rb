1352fbf08bf6351ba852b2c1f85bcea5b1e1352f
shawniut <shawniut@gmail.com>
2014-05-14 16:30:34 -0600

    navigation menu added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RELREA::Application.routes.draw do
+
+  resources :home
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
