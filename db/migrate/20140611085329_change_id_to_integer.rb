3848b7c3696bd7612293c093a0f260560538f44d
shawniut <shawniut@gmail.com>
2014-06-11 03:21:51 -0600

    id changed into string to integer

--- db/migrate/20140611085329_change_id_to_integer.rb
+++ db/migrate/20140611085329_change_id_to_integer.rb
@@ -5,7 +5,7 @@ class ChangeIdToInteger < ActiveRecord::Migration
       t.change :metric_id, 'integer USING CAST(metric_id AS integer)'
       t.change :mfunction_id, 'integer USING CAST(mfunction_id AS integer)'
       t.change :raw_file_id, 'integer USING CAST(raw_file_id AS integer)'
-    
+    end
     change_table :infos do |t|
       t.change :project_id, 'integer USING CAST(project_id AS integer)'
     end
@@ -21,27 +21,27 @@ class ChangeIdToInteger < ActiveRecord::Migration
      t.change :project_id, 'integer USING CAST(project_id AS integer)'
     end
   end
-  def self.down
-    change_table :raw_files do |t|
-      t.change :project_id, :string
-      t.change :metric_id, :string
-      t.change :mfunction_id, :string
-      t.change :raw_file_id, :string
-    end
+  # def self.down
+  #   change_table :raw_files do |t|
+  #     t.change :project_id, :string
+  #     t.change :metric_id, :string
+  #     t.change :mfunction_id, :string
+  #     t.change :raw_file_id, :string
+  #   end
 
-    change_table :infos do |t|
-      t.change :project_id, :string
-    end
-    change_table :raw_files do |t|
-     t.change :rattribute_id, :string
-    end
+  #   change_table :infos do |t|
+  #     t.change :project_id, :string
+  #   end
+  #   change_table :raw_files do |t|
+  #    t.change :rattribute_id, :string
+  #   end
      
-    change_table :values do |t|
-     t.change :rattribute_id, :string
-    end
+  #   change_table :values do |t|
+  #    t.change :rattribute_id, :string
+  #   end
     
-    change_table :releases do |t|
-     t.change :project_id, :string
-    end
-  end
+  #   change_table :releases do |t|
+  #    t.change :project_id, :string
+  #   end
+  # end
 end
