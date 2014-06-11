8b63648424612e2ae95027c7218e0957668f3c4d
shawniut <shawniut@gmail.com>
2014-06-11 03:14:18 -0600

    id changed into string to integer

--- db/migrate/20140611085329_change_id_to_integer.rb
+++ db/migrate/20140611085329_change_id_to_integer.rb
@@ -1,25 +1,24 @@
 class ChangeIdToInteger < ActiveRecord::Migration
     def self.up
     change_table :rattributes do |t|
-      t.change :project_id, :integer
-      t.change :metric_id, :integer
-      t.change :mfunction_id, :integer
-      t.change :raw_file_id, :integer
-    end
+      t.change :project_id, 'integer USING CAST(project_id AS integer)'
+      t.change :metric_id, 'integer USING CAST(metric_id AS integer)'
+      t.change :mfunction_id, 'integer USING CAST(mfunction_id AS integer)'
+      t.change :raw_file_id, 'integer USING CAST(raw_file_id AS integer)'
     
     change_table :infos do |t|
-      t.change :project_id, :integer
+      t.change :project_id, 'integer USING CAST(project_id AS integer)'
     end
     change_table :raw_files do |t|
-     t.change :rattribute_id, :integer
+     t.change :rattribute_id, 'integer USING CAST(rattribute_id AS integer)'
     end
      
     change_table :values do |t|
-     t.change :rattribute_id, :integer
+     t.change :rattribute_id, 'integer USING CAST(rattribute_id AS integer)'
     end
     
     change_table :releases do |t|
-     t.change :project_id, :integer
+     t.change :project_id, 'integer USING CAST(project_id AS integer)'
     end
   end
   def self.down
