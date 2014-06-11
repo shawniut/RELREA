4c67d0118ed0d3b29e9f8a90d33872e55a675c78
shawniut <shawniut@gmail.com>
2014-06-11 02:06:04 -0600

    db seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Dataservice::Metric.create :name => "Feature completion rate", :source => "Github", :dimension => "Implementation"
+Dataservice::Metric.create :name => "Features Implemented", :source => "Github", :dimension => "Implementation"
+Dataservice::Metric.create :name => "Build success rate ", :source => "Travis-CI", :dimension => "Implementation"
+Dataservice::Metric.create :name => "Code churn rate", :source => "Github", :dimension => "Testing"
+Dataservice::Metric.create :name => "Defect find rate", :source => "Github", :dimension => "Testing"
+Dataservice::Metric.create :name => "Bug fix rate", :source => "Github", :dimension => "Testing"
+Dataservice::Metric.create :name => "Pull-request Completion Rate", :source => "Github", :dimension => "Testing"
