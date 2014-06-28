e8ea61567515f3cb14129ed2269dfa6a4bd53d9a
shawniut <shawniut@gmail.com>
2014-06-28 00:41:00 -0600

    RR analysis is updated

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -10,15 +10,24 @@ class AnalysisController < ApplicationController
       @selected_index = 0
       @moving_average_index = 0
       @moving_average_order = 3
+      @analysis_type = 1
+      @analysis_type_index = 0
     else
       release_id = params["release_id"].to_i
       @Releases =  release_id!=0 ? Release.where(:id =>release_id) : @Project.releases
       @selected_index = params["selected_index"]
     end
+
     if params["moving_average_selected_index"] != nil
       @moving_average_order =  params["moving_average_id"].to_i
       @moving_average_index = params["moving_average_selected_index"]
     end
+
+    if params["analysis_type_id"] != nil
+       @analysis_type = params["analysis_type_id"].to_i
+       @analysis_type_index = @analysis_type -1
+    end
+
   	respond_to do |format|	 	
 			format.js	
 	end
